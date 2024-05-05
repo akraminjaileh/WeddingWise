84143481b71038d54fce9b93ed6fd95d85dc49c8
Akram Injaileh <akraminjaileh@gmail.com>
2024-05-05 08:10:44 +0300

    Agent

---
+++ WeddingWise/Controllers/AgentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WeddingWise_Core.DTO.CarRental;
+using WeddingWise_Core.Helper;
+using WeddingWise_Core.IServices;
+
+namespace WeddingWise.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgentController : ControllerBase
+    {
+        private readonly IAgentServices services;
+
+        public AgentController(IAgentServices services)
+        {
+            this.services = services;
+        }
+
+        [HttpPost]
+        [Route("[Action]")]
+        public async Task<IActionResult> GetAllTransaction([FromHeader] string token)
+        {
+            var claims = await JWTDecoding.JWTDecod(token);
+
+            return Ok(await services.GetAllTransaction(claims));
+        }
+        
+    }
+}
